889f70ec74e681c7c8a315e639425249c64391a3
PYPARNs <piyaparn40541@gmail.com>
2022-03-15 21:52:59 +0700

    remove: delete comment.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './pages/App';
 import { BrowserRouter } from 'react-router-dom';
-// import { PostsProvider } from './store/PostsProvider';
 
 const AppWithRouter = () => (
   <BrowserRouter>
-    {/* <PostsProvider> */}
       <App />
-    {/* </PostsProvider> */}
   </BrowserRouter>
 )
 
